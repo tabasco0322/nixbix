3c48312d908ff5dbf7a2d46d02ddff58237226e4
tabasco0322 <example@example.com>
2025-11-13 17:10:59 +0100

    Revert openssh_10_2

--- users/profiles/ssh.nix
+++ users/profiles/ssh.nix
@@ -1,12 +1,10 @@
 {
-  pkgs,
+  _,
   ...
 }:
 {
   programs.ssh = {
     enable = true;
-    # temp fix since 10.1p1 was super slow, remember to delete pkgs import
-    package = pkgs.openssh_10_2;
     enableDefaultConfig = false;
 
     matchBlocks = {
@@ -20,8 +18,8 @@
         forwardAgent = true;
         addKeysToAgent = "no";
         compression = false;
-    };
-    "github github.com" = {
+      };
+      "github github.com" = {
         hostname = "github.com";
         user = "git";
         forwardAgent = false;
